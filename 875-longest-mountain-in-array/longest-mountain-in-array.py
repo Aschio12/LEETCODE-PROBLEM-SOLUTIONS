3dda87730afd4ac6862f9af3a46890da9acbc92e
Aschio12 <aschalewdere2@gmail.com>
2025-07-22 06:30:00 -0700

    Time: 14 ms (42.83%) | Memory: 19 MB (14.52%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.py
+++ 875-longest-mountain-in-array/longest-mountain-in-array.py
@@ -1,14 +1,21 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
         res=0
-        for i in range(1,len(arr)-1):
+        i=1
+        while i <len(arr)-1:
+            c=False
             if arr[i-1]<arr[i]>arr[i+1]:
                 l,r=i,i
                 while l>0 and arr[l]>arr[l-1]:
                     l-=1
                 while  r+1<len(arr) and arr[r]>arr[r+1]:
                     r+=1
+                    c=True
                 res=max(res,(r-l+1))
+            if c:
+                i=r+1
+            else:
+                i+=1
         return res
             
                 
