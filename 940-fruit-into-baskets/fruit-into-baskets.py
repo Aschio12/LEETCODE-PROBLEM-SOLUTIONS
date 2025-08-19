b23ed25bf24b7e682e359071dfc8b24878bbcd58
Aschio12 <aschalewdere2@gmail.com>
2025-08-19 08:28:08 -0700

    Time: 195 ms (29.37%) | Memory: 23.9 MB (6.20%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count,ans,l=2,0,0
+        d=defaultdict(int)
+        for r in range(len(fruits)):
+            if d[fruits[r]]==0:
+                count-=1
+            d[fruits[r]]+=1
+            while count<0:
+                d[fruits[l]]-=1
+                if d[fruits[l]]==0:
+                    count+=1
+                l+=1
+            ans=max(ans,r-l+1)
+        return ans
+                
