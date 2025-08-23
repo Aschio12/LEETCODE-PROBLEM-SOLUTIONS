08f625300c18cea88a8857e5b23f450c644819ff
Aschio12 <aschalewdere2@gmail.com>
2025-08-23 00:02:19 -0700

    Time: 30 ms (31.38%) | Memory: 28.7 MB (89.68%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,11 +1,14 @@
-
+from collections import defaultdict
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        seen,ans=set(),set()
+        ans,seen=[],defaultdict(int)
         for l in range(len(s)-9):
             current=s[l:l+10]
-            if current in seen:
-                ans.add(current)
-            seen.add(current)
-        return list(ans)
+            seen[current]+=1
+
+        for key,val in seen.items():
+            if val>1:
+                ans.append(key)
+    
+        return ans
 
