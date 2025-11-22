25cd490334a8120b4eecd46c9910e8a7bfda3748
Aschio12 <aschalewdere2@gmail.com>
2025-11-21 23:18:42 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (77.56%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,8 +1,9 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         expected=sorted(heights)
-        ans=0
-        for i in range(len(heights)):
-            if expected[i]!=heights[i]:
-                ans+=1
-        return ans
+        inordered=0
+        for i1,i2 in zip(expected,heights):
+            if i1!=i2:
+                inordered+=1
+        return inordered
+
