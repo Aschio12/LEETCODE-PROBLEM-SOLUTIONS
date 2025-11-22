2b2541ed36c3ac3f7712f5d4e6802e2da580d43c
Aschio12 <aschalewdere2@gmail.com>
2025-11-22 07:03:20 -0800

    Time: 4 ms (26.79%) | Memory: 17.9 MB (41.67%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -2,15 +2,15 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         a1=Counter(arr1)
         a2=set(arr2)
-        not_a1=[]
+        not_a1=set()
         for n in arr1:
-            if n not in a2 and n not in not_a1:
-                not_a1.append(n)
+            if n not in a2 :
+                not_a1.add(n)
         ans=[]
-        not_a1.sort()
+        not_a11=sorted(list(not_a1))
         for i in arr2:
             ans.extend([i]*a1[i])
-        for j in not_a1:
+        for j in not_a11:
             ans.extend([j]*a1[j])
         return ans
         
