d6a935a53f8547453877e9142475f011aee39584
Aschio12 <aschalewdere2@gmail.com>
2025-12-14 23:34:48 -0800

    Time: 91 ms (5.29%) | Memory: 33 MB (5.07%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -1,14 +1,18 @@
 class Solution:
-    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        mark=[0]*200001
+    def corpFlightBookings(self, bookings: List[List[int]], m: int) -> List[int]:
+        n=200000
+        mark=[0]*n
+        minn,maxx=n,1
         for first,last,seats in (bookings):
             mark[first]+=seats
             mark[last+1]-=seats
-        prefix=[0]*200001
+            maxx=max(maxx,last)
+            minn=min(minn,first)
+        prefix=[0]*n
         prefix[0]=mark[0]
-        for i in range(1,len(prefix)):
+        for i in range(minn,maxx+1):
             prefix[i]=mark[i]+prefix[i-1]
         ans=[]
-        for i in range(1,n+1):
+        for i in range(1,m+1):
             ans.append(prefix[i])
         return ans
