2e82133f72e08a9be8da288e76f4631e0ebfd859
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-02-04 01:03:14 -0800

    Time: 0 ms (100.00%) | Memory: 21.4 MB (26.51%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,15 +1,25 @@
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals.sort()
-        ans=[]
-        start,end=intervals[0][0],intervals[0][1]
-        for char in range(1,len(intervals)):
-            left,right=intervals[char][0],intervals[char][1]
-            if left<=end:
-                end=max(end,right)
-            else:
-                ans.append([start,end])
-                start,end = left,right
-        ans.append([start,end])        
+    def insert(self, interval: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        i,ans=0,[]
+        n=len(interval)
+        while i<n and interval[i][1]<newInterval[0]:
+            ans.append(interval[i])
+            i+=1
+        while i<n and newInterval[1]>=interval[i][0]:
+            newInterval[0]=min(newInterval[0],interval[i][0])
+            newInterval[1]=max(newInterval[1],interval[i][1])
+            i+=1
+        ans.append(newInterval)
+        while i<n:
+            ans.append(interval[i])
+            i+=1
         return ans
+        
+        
+
+        
+            
+
+
+
+
