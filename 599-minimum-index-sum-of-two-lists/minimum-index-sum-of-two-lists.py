67ed2029d9c0dab2205c0cdb8f5b03c4703154ca
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-02-05 05:32:10 -0800

    Time: 1 ms (96.27%) | Memory: 19.8 MB (34.73%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,29 +1,17 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        list11,list22=[],[]
-        for i in range(len(list1)):
-            list11.append((list1[i],i))
-        for j in range(len(list2)):
-            list22.append((list2[j],j))
-        list11.sort()
-        list22.sort()
-        l1,l2=0,0
-        d={}
-        m=2002
-        while l1<len(list11) and l2<len(list22):
-            if list11[l1][0]==list22[l2][0]:
-                m=min(m,list11[l1][1]+list22[l2][1])
-                d[list11[l1][0]]=list11[l1][1]+list22[l2][1]
-                l2+=1
-                l1+=1
-            elif list11[l1][0]>list22[l2][0]:
-                l2+=1
-            else:
-                l1+=1
+        list11={word:i for i,word in enumerate(list1)}
+        m=float("inf")
         ans=[]
-        for key,val in d.items():
-            if val==m:
-                ans.append(key)
+        for index,val in enumerate(list2):
+            if val in list11:
+                tot=index+list11[val]
+                if tot<m:
+                    m=tot
+                    ans=[val]
+                elif tot==m:
+                    ans.append(val)
+            
         return ans
 
 
