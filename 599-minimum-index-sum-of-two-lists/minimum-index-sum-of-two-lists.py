1543f796a6d7d4ccc5ab95f48be26cc7e26252a3
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-02-05 05:18:32 -0800

    Time: 31 ms (38.61%) | Memory: 20 MB (15.53%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,18 +1,30 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        index_map = {word: i for i, word in enumerate(list1)}
-        min_sum = float('inf')
-        result = []
+        list11,list22=[],[]
+        for i in range(len(list1)):
+            list11.append((list1[i],i))
+        for j in range(len(list2)):
+            list22.append((list2[j],j))
+        list11.sort()
+        list22.sort()
+        l1,l2=0,0
+        d={}
+        m=2002
+        while l1<len(list11) and l2<len(list22):
+            if list11[l1][0]==list22[l2][0]:
+                m=min(m,list11[l1][1]+list22[l2][1])
+                d[list11[l1][0]]=list11[l1][1]+list22[l2][1]
+                l2+=1
+                l1+=1
+            elif list11[l1][0]>list22[l2][0]:
+                l2+=1
+            else:
+                l1+=1
+        ans=[]
+        for key,val in d.items():
+            if val==m:
+                ans.append(key)
+        return ans
 
-        for j, word in enumerate(list2):
-            if word in index_map:
-                i = index_map[word]
-                total = i + j
 
-                if total < min_sum:
-                    min_sum = total
-                    result = [word]  # start fresh
-                elif total == min_sum:
-                    result.append(word)
 
-        return result
