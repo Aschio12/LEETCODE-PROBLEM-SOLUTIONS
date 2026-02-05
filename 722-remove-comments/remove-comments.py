d28a8bf541387d48ff5ed21e0b952bbafe3518c7
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-02-05 04:26:57 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.90%) - LeetSync

---
+++ 722-remove-comments/remove-comments.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        ans=[]
+        blocked=False
+        for char in source:
+            i=0
+            if not blocked:
+                current=""
+            while i<len(char):
+                if not blocked:
+                    if i+1<len(char) and char[i:i+2]=="//":
+                        break
+                    if i+1<len(char) and char[i]+char[i+1]=="/*":
+                        blocked=True
+                        i+=1
+                    else:
+                        current+=char[i]
+                        
+                else:
+                    if i+1<len(char) and char[i]+char[i+1]=="*/":
+                        blocked=False
+                        i+=1
+                i+=1
+                  
+            if current and not blocked:
+                ans.append(current)
+                current=""
+        return ans
+        
