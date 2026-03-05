82fb5c8b7b853a5e15296505e23a956abd228aa9
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-03-05 15:54:58 +0300

    Time: 36 ms (41.96%) | Memory: 21.8 MB (80.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,14 +1,14 @@
-from collections import Counter
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        d,current={0:1},0
-        count=0
-        for num in nums:
-            current+=num
-            if current-k in d:
-                count+=d[current-k]
-            if current in d:
-                d[current]+=1
+        pre={0:1}
+        p,count=0,0
+        for i in range(len(nums)):
+            p+=nums[i]
+            if p-k in pre:
+                count+=pre[p-k]
+            if p in pre:
+                pre[p]+=1
             else:
-                d[current]=1
+                pre[p]=1
         return count
+
