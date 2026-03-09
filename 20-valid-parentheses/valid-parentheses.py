69a8b133581e71fc960e40720a9036a13f82930a
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-03-09 18:34:09 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        close={"]":"[","}":"{",")":"("}
+
+        close={")":"(","]":'[',"}":"{"}
+
         stack=[]
+
         for ch in s:
             if ch in close:
-                if stack and close[ch]==stack[-1]:
-                    stack.pop()
+                if stack:
+                    if close[ch]==stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
                 else:
                     return False
+
             else:
                 stack.append(ch)
         return not stack
-
