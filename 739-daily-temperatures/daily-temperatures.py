930bf20c44d44e0abf6703af1d1f0cfa0a3f5613
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-03-10 14:33:10 +0300

    Time: 231 ms (5.05%) | Memory: 44.2 MB (5.04%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,10 +1,15 @@
 class Solution:
-    def dailyTemperatures(self, tem: List[int]) -> List[int]:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        arr=[[val,index] for index,val in enumerate(temperatures)]
         stack=[]
-        ans=[0]*len(tem)
-        for index,val in enumerate(tem):
-            while stack and val>tem[stack[-1]]:
-                prev=stack.pop()
-                ans[prev]=index-prev
-            stack.append(index)
+        ans=[0]*(len(arr))
+
+        for val,index in arr:
+            while stack and val>stack[-1][0]:
+                ans[stack[-1][1]]=(index)-stack[-1][-1]
+                stack.pop()
+            if not stack or val<=stack[-1][0]:
+                stack.append([val,index])
+            
         return ans
+
