6d10e78d5b04695793592a58ac056bbb4db35057
Aschalew Daraje <aschalew.daraje@astu.edu.et>
2026-03-31 12:23:38 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.45%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        self.res=0
+        def rec(node):
+            if not node:
+                return 0
+
+            left=rec(node.left)
+            right=rec(node.right)
+
+            self.res+=abs(left)+abs(right)
+            return left+right+node.val-1
+        rec(root)
+        return self.res
+        
